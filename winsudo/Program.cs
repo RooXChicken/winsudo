7adb88ea32f578f92d4b5558ae944ded95f9a76e
RooXChicken <57197457+RooXChicken@users.noreply.github.com>
2021-10-30 16:52:49 -0400

    Add files via upload

---
+++ winsudo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace winsudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+
+            foreach(string s in args)
+            {
+                command += s + " ";
+            }
+
+            if(command == "")
+            {
+                Console.WriteLine("Provide a command dummy!");
+            }
+            else
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.UseShellExecute = true;
+                info.Verb = "runas";
+                info.WorkingDirectory = @"C:\Windows\System32";
+                info.FileName = @"C:\Windows\System32\cmd.exe";
+                info.Arguments = "/C" + command + " && pause";
+
+                try
+                {
+                    Process.Start(info);
+                    Console.WriteLine($"Running command as administrator!");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Failed to run command as administrator. No permission!");
+                }
+            }
+        }
+    }
+}
